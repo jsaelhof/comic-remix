1a6752bb08cbc4018f8249c911ad24b47c2fbe0b
Jason Saelhof <jsaelhof@gmail.com>
2023-08-14 08:09:38 -0600

    Add remix i18n

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,16 @@
 import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration, useLoaderData
 } from "@remix-run/react";
+import i18next from "~/i18next.server";
+import { useChangeLanguage } from "remix-i18next";
+import { useTranslation } from "react-i18next";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
@@ -27,14 +30,33 @@ export const meta: MetaFunction = () => ({
 });
 
 export async function loader({ request }: LoaderArgs) {
-  return json({
-    user: await getUser(request),
-  });
+  let locale = await i18next.getLocale(request);
+  return json({ locale, user: await getUser(request), });
 }
 
+export let handle = {
+  // In the handle export, we can add a i18n key with namespaces our route
+  // will need to load. This key can be a single string or an array of strings.
+  // TIP: In most cases, you should set this to your defaultNS from your i18n config
+  // or if you did not set one, set it to the i18next default namespace "translation"
+  i18n: "common",
+};
+
+
 export default function App() {
+  // Get the locale from the loader
+  let { locale } = useLoaderData<typeof loader>();
+
+  let { i18n } = useTranslation();
+
+  // This hook will change the i18n instance language to the current locale
+  // detected by the loader, this way, when we do something to change the
+  // language, this locale will change and i18next will load the correct
+  // translation files
+  useChangeLanguage(locale);
+
   return (
-    <html lang="en" className="h-full">
+    <html lang={locale} dir={i18n.dir()} className="h-full">
       <head>
         <Meta />
         <Links />
--- app/routes/comics.tsx
+++ app/routes/comics.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "@remix-run/react";
 import AppBar from "~/components/app-bar/app-bar";
 import styles from "./comics.module.css";
+import { useTranslation } from "react-i18next";
 
 const Comics = () => {
+  let { t } = useTranslation();
+
   return (
     <div className={styles.mainLayout}>
       <AppBar />
 
       <div className={styles.content}>
+        <h1>{t("greeting")}</h1>
         <Outlet />
       </div>
     </div>
